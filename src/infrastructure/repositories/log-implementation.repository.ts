273b7febef04848923d3f6789faa0631d3c45d73
Alex Oliva <alexoliva87464@gmail.com>
2024-07-29 05:47:56 -0300

    feat: Add LogRepositoryImplementation for logging functionality

---
+++ src/infrastructure/repositories/log-implementation.repository.ts
@@ -0,0 +1,14 @@
+import { LogDataSource } from '../../domain/datasources/log.datasource';
+import { LogEntity, LogSeverityLevel } from '../../domain/entities';
+import { LogRepository } from '../../domain/repository';
+
+export class LogRepositoryImplementation implements LogRepository {
+  constructor(private readonly logDataSource: LogDataSource) {}
+
+  async saveLog(log: LogEntity): Promise<void> {
+    return this.logDataSource.saveLog(log);
+  }
+  async getLogs(severityLevel: LogSeverityLevel): Promise<LogEntity[]> {
+    return this.logDataSource.getLogs(severityLevel);
+  }
+}
