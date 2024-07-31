0a46abf19943e4c8a40ec754644101a6eb2dcafb
Alex Oliva <alexoliva87464@gmail.com>
2024-07-31 02:10:10 -0300

    feat: Implement SendEmailLogs use case for sending email logs

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,20 +1,23 @@
-import { CheckService } from '../domain/use-cases/checks';
+import { SendEmailLogs } from '../domain/use-cases/email/send-email-logs';
 import { FileSystemDataSource } from '../infrastructure/datasources/file-system.datasource';
 import { LogRepositoryImplementation } from '../infrastructure/repositories';
-import { CronService } from './cron';
+
 import { EmailService } from './email/email.service';
 
 const fileSystemLogRepository = new LogRepositoryImplementation(
   new FileSystemDataSource()
 );
+const emailService = new EmailService();
 
 export class Server {
   public static start() {
     console.log('Server started');
 
-    // TODO: Send mail
-    // const emailService = new EmailService(fileSystemLogRepository);
-
+    // TODO: Send email
+    // new SendEmailLogs(emailService, fileSystemLogRepository).execute([
+    //   'alexoliva.developer@gmail.com',
+    //   'alex.oliva.dev@gmail.com',
+    // ]);
     // emailService.sendEmailWithFileSystemLogs([
     //   'alexoliva.developer@gmail.com',
     //   'alex.oliva.dev@gmail.com',
