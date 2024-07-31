d3a866b63fdc63d969bafd8719802d7b88d154e7
Alex Oliva <alexoliva87464@gmail.com>
2024-07-31 01:14:26 -0300

    feat: Refactor LogEntity constructor to accept options object

--- src/domain/entities/log.entity.ts
+++ src/domain/entities/log.entity.ts
@@ -4,24 +4,39 @@ export enum LogSeverityLevel {
   high = 'high',
 }
 
+export interface LogEntityOptions {
+  level: LogSeverityLevel; // Enum
+  message: string;
+  createdAt?: Date;
+  origin: string;
+}
+
 export class LogEntity {
   public level: LogSeverityLevel; // Enum
   public message: string;
   public createdAt: Date;
+  public origin: string;
 
-  constructor(message: string, level: LogSeverityLevel) {
+  constructor(options: LogEntityOptions) {
+    const { message, level, origin, createdAt = new Date() } = options;
     this.message = message;
     this.level = level;
-    this.createdAt = new Date();
+    this.createdAt = createdAt;
+    this.origin = origin;
   }
 
   // This method is used to convert the LogEntity instance to a JSON string
   static fromJson(json: string): LogEntity {
-    const { message, level, createdAt } = JSON.parse(json);
+    const { message, level, createdAt, origin } = JSON.parse(json);
 
     if (!message || !level || !createdAt) throw new Error('Invalid JSON');
 
-    const log = new LogEntity(message, level);
+    const log = new LogEntity({
+      message,
+      level,
+      createdAt,
+      origin,
+    });
     log.createdAt = new Date(createdAt);
 
     return log;
