85f941075e7e1a8925dae0dca5272253f3d14c6a
Alex Oliva <alexoliva87464@gmail.com>
2024-08-01 13:07:48 -0300

    feat: Add MongoLogDatasource for saving and retrieving logs from MongoDB

---
+++ src/infrastructure/datasources/mongo-log.datasource.ts
@@ -0,0 +1,16 @@
+import { LogModel } from '../../data/mongo';
+import { LogDataSource } from '../../domain/datasources';
+import { LogEntity, LogSeverityLevel } from '../../domain/entities';
+
+export class MongoLogDatasource implements LogDataSource {
+  async saveLog(log: LogEntity): Promise<void> {
+    const newLog = await LogModel.create(log);
+    // console.log('Mongo Log saved:', newLog);
+  }
+
+  async getLogs(severityLevel: LogSeverityLevel): Promise<LogEntity[]> {
+    const logs = await LogModel.find({ level: severityLevel });
+
+    return logs.map(LogEntity.fromObject);
+  }
+}
