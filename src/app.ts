b36b1168de3c2491247f6f650a4e4e2b2e94eb32
Alex Oliva <alexoliva87464@gmail.com>
2024-08-01 10:01:09 -0300

    chore: Update app.ts to connect to MongoDB and retrieve logs

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,28 @@
+import { envs } from './config/plugins';
+import { LogModel, MongoDatabase } from './data/mongo';
 import { Server } from './presentation';
 
 (async () => {
   main();
 })();
 
-function main() {
-  Server.start();
+async function main() {
+  await MongoDatabase.connect({
+    mongoUrl: envs.MONGO_URL,
+    dbName: envs.MONGO_NAME,
+  });
+
+  // Create a new collection in the database
+  // const newLog = await LogModel.create({
+  //   message: 'Hello World',
+  //   origin: 'app.ts',
+  //   level: 'low',
+  // });
+
+  // await newLog.save();
+
+  const logs = await LogModel.find();
+  console.log(logs);
+
+  // Server.start();
 }
