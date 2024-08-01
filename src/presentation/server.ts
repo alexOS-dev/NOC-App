6ca8570b1696585f6b5af7224b07fb1cd4914790
Alex Oliva <alexoliva87464@gmail.com>
2024-08-01 13:07:52 -0300

    feat: Update server.ts to use MongoLogDatasource for log retrieval

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,16 +1,21 @@
+import { LogSeverityLevel } from '../domain/entities';
+import { CheckService } from '../domain/use-cases/checks';
 import { SendEmailLogs } from '../domain/use-cases/email/send-email-logs';
 import { FileSystemDataSource } from '../infrastructure/datasources/file-system.datasource';
+import { MongoLogDatasource } from '../infrastructure/datasources/mongo-log.datasource';
 import { LogRepositoryImplementation } from '../infrastructure/repositories';
+import { CronService } from './cron';
 
 import { EmailService } from './email/email.service';
 
-const fileSystemLogRepository = new LogRepositoryImplementation(
-  new FileSystemDataSource()
+const logRepository = new LogRepositoryImplementation(
+  // new FileSystemDataSource()
+  new MongoLogDatasource()
 );
 const emailService = new EmailService();
 
 export class Server {
-  public static start() {
+  public static async start() {
     console.log('Server started');
 
     // TODO: Send email
@@ -23,11 +28,13 @@ export class Server {
     //   'alex.oliva.dev@gmail.com',
     // ]);
 
+    const logs = await logRepository.getLogs(LogSeverityLevel.low);
+    console.log(logs);
     // CronService.createJob('*/5 * * * * *', () => {
     //   const url = 'https://www.google.com';
     //   // const url = 'http//localhost:3000';
     //   new CheckService(
-    //     fileSystemLogRepository,
+    //     logRepository,
     //     () => console.log(`${url} is ok`),
     //     (error) => console.log(error)
     //   ).execute(url);
