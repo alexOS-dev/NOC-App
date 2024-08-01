e9d9c5206a3a70a7945a2707e6bc0a820412829b
Alex Oliva <alexoliva87464@gmail.com>
2024-08-01 14:02:08 -0300

    feat: Add PostgresLogDatasource for saving and retrieving logs from PostgreSQL

---
+++ src/infrastructure/datasources/postgres-log.datasource.ts
@@ -0,0 +1,37 @@
+import { PrismaClient, SeverityLevel } from '@prisma/client';
+
+import { LogEntity, LogSeverityLevel } from '../../domain/entities/log.entity';
+import { LogDataSource } from '../../domain/datasources';
+
+const prismaClient = new PrismaClient();
+
+const severityEnum = {
+  low: SeverityLevel.LOW,
+  medium: SeverityLevel.MEDIUM,
+  high: SeverityLevel.HIGH,
+};
+
+export class PostgresLogDatasource implements LogDataSource {
+  async saveLog(log: LogEntity): Promise<void> {
+    const level = severityEnum[log.level];
+
+    const newLog = await prismaClient.logModel.create({
+      data: {
+        ...log,
+        level: level,
+      },
+    });
+
+    console.log('Posgres saved');
+  }
+
+  async getLogs(severityLevel: LogSeverityLevel): Promise<LogEntity[]> {
+    const level = severityEnum[severityLevel];
+
+    const dbLogs = await prismaClient.logModel.findMany({
+      where: { level },
+    });
+
+    return dbLogs.map(LogEntity.fromObject);
+  }
+}
