6b4efdf90d5dc7b5675f351e4883a09475f25733
Alex Oliva <alexoliva87464@gmail.com>
2024-08-02 07:57:12 -0300

    chore: Remove console.log statements in MongoDB initialization code

--- src/data/mongo/init.ts
+++ src/data/mongo/init.ts
@@ -14,9 +14,9 @@ export class MongoDatabase {
         dbName,
       });
 
-      console.log('Connected to MongoDB');
+      // console.log('Connected to MongoDB');
+      return true;
     } catch (error) {
-      console.log('Error connecting to MongoDB');
       throw error;
     }
   }
